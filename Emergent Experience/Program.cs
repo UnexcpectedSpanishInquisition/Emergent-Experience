82982560a03c357516dc5a4480a5702f3a349321
Josh Deller <josh.deller@citysurveysgroup.co.uk>
2021-02-10 07:58:02 -0800

    First working Version V0.002

---
+++ Emergent Experience/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace Emergent_Experience
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        => await Startup.RunAsync(args);
+    }
+}
